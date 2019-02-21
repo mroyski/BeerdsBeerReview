2b497c9f2b080c9563ce8cce4e173554cefa9325
Mike <mroyski@gmail.com>
2019-02-21 11:36:13 -0500

    no tests

--- ReviewsSites/Controllers/ReviewsController.cs
+++ ReviewsSites/Controllers/ReviewsController.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ReviewsSites.Controllers
 {
-    public class ReviewsController
+    public class ReviewsController : Controller
     {
+
+
     }
 }
