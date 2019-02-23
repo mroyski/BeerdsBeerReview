0375cd810f63dfeb9d4c45b86d034595e6189fb1
Mike <mroyski@gmail.com>
2019-02-22 20:48:04 -0500

    fixed details css

--- ReviewsSites/Repositories/ReviewsRepository.cs
+++ ReviewsSites/Repositories/ReviewsRepository.cs
@@ -10,7 +10,7 @@ namespace ReviewsSites.ReviewsRepositories
     {
         public Dictionary<int, Reviews> reviewList = new Dictionary<int, Reviews>()
             {
-                {1, new Reviews() {Id = 1, Beer = "Zombie Dust", Rating = 4.8f, Description = "blah blah", ImgPath = "/Images/Zombiedust.png" } },
+                {1, new Reviews() {Id = 1, Beer = "Zombie Dust", Rating = 4.8f, Description = "This intensely hopped and gushing undead Pale Ale will be one’s only respite after the zombie apocalypse. Created with our marvelous friends in the comic industry.", ImgPath = "/Images/Zombiedust.png" } },
                 {2, new Reviews() {Id = 2, Beer = "Hopslam", Rating = 4.6f, Description = "blah blah"} },
                 {3, new Reviews() {Id = 3, Beer = "Boomsauce", Rating = 3.0f, Description = "blah blah"} }
             };
