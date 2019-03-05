9c483823566df1d00dbeb9be8d256af445bf9b89
Mike <mroyski@gmail.com>
2019-03-05 10:05:33 -0500

    tweaked

--- ReviewsSites/Controllers/UserReviewController.cs
+++ ReviewsSites/Controllers/UserReviewController.cs
@@ -50,7 +50,7 @@ namespace ReviewsSites.Controllers
         public ActionResult Delete(UserReview userReview)
         {
             userReviewRepo.Delete(userReview);
-            return RedirectToAction("../Beer/Index");
+            return RedirectToAction("../Beer/Index/");
         }
 
         [HttpGet]
