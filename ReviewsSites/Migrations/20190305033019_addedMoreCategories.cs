6b6ee63cded79a3ce09f7b4b5fa2c9b2671b863a
Bpjd21 <bpjd21@yahoo.com>
2019-03-04 22:58:36 -0500

    added more categories

---
+++ ReviewsSites/Migrations/20190305033019_addedMoreCategories.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewsSites.Migrations
+{
+    public partial class addedMoreCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "DetailDescription", "ImgPath", "Style" },
+                values: new object[] { 2, "A stout beer is a dark, sometimes bitter beer that was first commercially produced in the 1730s. The first versions of this drink were from Ireland, where the still most popular stout is produced by Guinness. Yet Guinness is only one of the many companies that produce and market stouts.", "/Images/Stout.png", "Stout" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "DetailDescription", "ImgPath", "Style" },
+                values: new object[] { 3, "Pale lager is a very pale-to-golden-colored lager beer with a well attenuated body and a varying degree of noble hop bitterness.", "/Images/lager.png", "Pale Lager" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+        }
+    }
+}
---
+++ ReviewsSites/Migrations/20190305035430_AddedForthCat.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewsSites.Migrations
+{
+    public partial class AddedForthCat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3,
+                columns: new[] { "DetailDescription", "Style" },
+                values: new object[] { "Pale lager is a very pale - to - golden - colored lager beer with a well attenuated body and a varying degree of noble hop bitterness.", "Lager" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "DetailDescription", "ImgPath", "Style" },
+                values: new object[] { 4, "Pale lager is a very pale-to-golden-colored lager beer with a well attenuated body and a varying degree of noble hop bitterness.", "/Images/PaleAle.png", "Pale Ale" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3,
+                columns: new[] { "DetailDescription", "Style" },
+                values: new object[] { "Pale lager is a very pale-to-golden-colored lager beer with a well attenuated body and a varying degree of noble hop bitterness.", "Pale Lager" });
+        }
+    }
+}
